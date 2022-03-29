b154fea71d0d8a276899a12c26cb2f9df68c5c78
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2022-03-29 18:36:46 -0300

    add post service

--- src/bucket/bucket.controller.ts
+++ src/bucket/bucket.controller.ts
@@ -1,24 +1,29 @@
 import { Controller, Param, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { BucketService } from './bucket.service';
 import { imageFileFilter } from '../utils/image-uploading.utils';
 import { UploadImageResponseDTO } from './dto/upload-image-response';
+import { UploadImageDTO } from './dto/upload-image.dto';
 
 @Controller('bucket')
 export class BucketController {
   constructor(private readonly bucketService: BucketService) {}
 
-  // TODO
-  // @Post(':userId/images')
-  // @UseInterceptors(
-  //   FilesInterceptor('images', null, {
-  //     fileFilter: imageFileFilter,
-  //   }),
-  // )
-  // async uploadImages(
-  //   @Param('userId') userId: string,
-  //   @UploadedFiles() files: Array<Express.Multer.File>,
-  // ): Promise<UploadImageResponseDTO> {
-  //   return await this.bucketService.put(userId, files);
-  // }
+  @Post(':userId/images')
+  @UseInterceptors(
+    FileInterceptor(
+      'image',
+      {
+        fileFilter: imageFileFilter,
+      }
+    ),
+  )
+  async uploadImages(
+    @Param('userId') userId: string,
+    @UploadedFiles() file: Express.Multer.File,
+  ): Promise<UploadImageResponseDTO> {
+    const uploadImageDTO = new UploadImageDTO;
+    uploadImageDTO.setValues(userId, file);
+    return await this.bucketService.put(uploadImageDTO);
+  }
 }
--- src/bucket/dto/upload-image.dto.ts
+++ src/bucket/dto/upload-image.dto.ts
@@ -4,4 +4,10 @@ export class UploadImageDTO {
   mimeType: string;
 
   user_id: string;
+
+  setValues(userId: string, file: Express.Multer.File) {
+    this.base64 = file.buffer.toString('base64');
+    this.mimeType = file.mimetype;
+    this.user_id = userId;
+  }
 }
